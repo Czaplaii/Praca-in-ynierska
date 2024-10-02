b51e0a83d84d17a02722398b07e24b4efca480e2
Czaplaii <czapla.jakub2@gmail.com>
2024-10-02 15:41:06 +0200

    wybieranie numerów, 1sza plansza

---
+++ Assets/Scripts/banner_bar.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class banner_bar : MonoBehaviour
+{
+    [SerializeField] Button[] Buttons;
+
+    void Start()
+    {
+        PlayerPrefs.SetInt("number", -1);
+        for (int i = 0; i < Buttons.Length; i++)
+        {
+            int index = i;
+            Buttons[i].onClick.AddListener(() => OnButtonClick(index));
+        }
+
+        int last = PlayerPrefs.GetInt("number");
+    }
+
+    void OnButtonClick(int index)
+    {
+        int last = PlayerPrefs.GetInt("number", index);
+        if (last != -1)
+        {
+            Buttons[last].interactable = true;
+        }
+        Buttons[index].interactable = false;
+        PlayerPrefs.SetInt("number", index);
+        Debug.Log($"Klikniêto przycisk {index + 1}. Wy³¹czono.");
+        Debug.Log($"Playerprefs {PlayerPrefs.GetInt("number")}.");
+    }
+}
