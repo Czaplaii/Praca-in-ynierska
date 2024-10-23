860c2ae8a4b5bf11735fe333f97c5ad8ece65f04
Czaplaii <czapla.jakub2@gmail.com>
2024-10-23 17:52:59 +0200

    uzupełnienie tablicy, skrypt do wypełniania
    Wadliwy skrypt do uzupełniania planszy sudoku, zmiana logiki uzupełniania ze startu, do korutyny; zmiana zasad losowania

--- Assets/Scripts/Board_creator.cs
+++ Assets/Scripts/Board_creator.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System.Collections;
 
 public class Board_creator : MonoBehaviour
 {
@@ -11,58 +10,64 @@ public class Board_creator : MonoBehaviour
 
     void Start()
     {
-        BoardInit(Board);
-        BoardCreate(Board);
+        StartCoroutine(CreateBoard());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator CreateBoard()
     {
-        
+        BoardInit(Board);
+        for(int answer = 1; answer<=9; answer++)
+        {
+            int placedcounter = 0;
+            while (placedcounter <9) // ile zmieœci siê konkretnych liczb na planszy
+            {
+                bool taken = false;
+                while (!taken && placedcounter < 9)
+                {
+                    int RandomRow = Random.Range(0, 9);
+                    int RandomColumn = Random.Range(0, 9);
+
+                    if (Board[RandomRow, RandomColumn] == 0)
+                    {
+                        if (!IsInRow(RandomRow, answer) && !IsInColumn(RandomColumn, answer) && !IsInSector(RandomRow, RandomColumn, answer))
+                        {
+                            Board[RandomRow, RandomColumn] = answer;
+
+                            int buttonIndex = (9 * RandomRow) + RandomColumn;
+                            TMP_Text buttonText = BoardButtons[buttonIndex].GetComponentInChildren<TMP_Text>();
+                            buttonText.text = answer.ToString();
+
+                            taken = true;
+                            placedcounter++;
+                        }
+                    }
+                    else
+                        continue;  
+                    yield return null;
+                }
+            }
+        }
     }
 
-    void BoardInit(int[,] tab)
+    void BoardInit(int[,] tab) //wype³niamy tablice zerami
     {
         for (int i = 0; i < Board.GetLength(0); i++)
         {
             for (int j = 0; j < Board.GetLength(1); j++)
             {
                 Board[i, j] = 0;
-                //wype³nianie tablicy
                 int buttonIndex = (9 * i) + j;
                 TMP_Text buttonText = BoardButtons[buttonIndex].GetComponentInChildren<TMP_Text>();
-                //buttonText.text = Board[i, j].ToString();
                 buttonText.text = " ";
-                // Wyœwietlenie wartoœci
-                Debug.Log("Wartoœæ w Board[" + i + "," + j + "] = " + Board[i, j]);
-                Debug.Log("Tablica wype³niona zerami");
             }
         }
     }
 
-    void BoardCreate(int[,] tab)
-    {
-        int RandomRow = Random.Range(0, 9);
-        int RandomColumn = Random.Range(0, 9);
-        int RandomAnswer = Random.Range(1, 10);
-        if (!IsInRow(RandomRow, RandomAnswer) && !IsInColumn(RandomColumn,RandomAnswer) && !IsInSector(RandomRow,RandomColumn,RandomAnswer))
-        {
-            Board[RandomRow, RandomColumn] = RandomAnswer;
-            Debug.Log("posz³o");
-        }
-        Debug.Log("Wiersz " + (RandomRow+1));
-        Debug.Log("Kolumna " + (RandomColumn+1));
-        Debug.Log("Numer " + RandomAnswer);
-        int buttonIndex = (9 * RandomRow ) + RandomColumn;
-        TMP_Text buttonText = BoardButtons[buttonIndex].GetComponentInChildren<TMP_Text>();
-        buttonText.text = RandomAnswer.ToString();
-    }
-
-    bool IsInRow(int row, int number)
+    bool IsInRow(int row, int number) // czy wyst¹pi³a w wierszu?
     {
         for (int i = 0; i < Board.GetLength(1); i++)
         {
-            if(number == Board[i, row])
+            if (Board[row, i] == number)
             {
                 return true;
             }
@@ -70,34 +75,33 @@ public class Board_creator : MonoBehaviour
         return false;
     }
 
-    bool IsInColumn(int column, int number)
-    {
-        for (int j = 0; j < Board.GetLength(1); j++)
-        {
-            if (number == Board[column,j])
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool IsInSector(int column, int row, int number)
+    bool IsInColumn(int column, int number)// czy wyst¹pi³a w kolumnie?
     {
         for (int i = 0; i < Board.GetLength(0); i++)
         {
-            for (int j = 0; j < Board.GetLength(1); j++)
+            if (Board[i, column] == number)
             {
-                if((row/3)*3+(column/3) == (i / 3) * 3 + (j / 3))
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool IsInSector(int row, int column, int number) //czy wyst¹pi³a w kwadracie 9x9
+    {
+        int sectorRowStart = (row / 3) * 3;
+        int sectorColumnStart = (column / 3) * 3;
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (Board[sectorRowStart + i, sectorColumnStart + j] == number)
                 {
-                    if(number == Board[i,j])
-                    {
-                        return true;
-                    }
+                    return true;
                 }
             }
         }
         return false;
     }
-
 }
